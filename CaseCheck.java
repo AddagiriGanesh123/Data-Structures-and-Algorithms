8757699fbcb7b250b01c14f31234f2f16c1cd98b
AddagiriGanesh123 <150156459+AddagiriGanesh123@users.noreply.github.com>
2025-01-07 23:21:42 +0530

    Add files via upload

---
+++ CaseCheck.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class CaseCheck {
+    public static void main(String[] args) {
+        Scanner in= new Scanner(System.in);
+        System.out.println(in.next().trim().charAt(2));
+        // FOR EXAMPLE : IF YOU WANT PRINT THE SECOND INDEXED CHARACTER IN THE GIVEN INPUTR IN THE COMMAND LINE..
+        // AND TRIM IS USED TO REMOVE THE EXTRA SPACES FROM THE INPUT
+    }
+}
