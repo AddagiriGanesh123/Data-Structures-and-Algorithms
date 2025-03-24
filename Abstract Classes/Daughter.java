e401f846c3d93bd8e1ffaba37e148f989131bc0f
AddagiriGanesh123 <150156459+AddagiriGanesh123@users.noreply.github.com>
2025-03-24 18:42:20 +0530

    Add files via upload

---
+++ Abstract Classes/Daughter.java
@@ -0,0 +1,16 @@
+package Abstraction;
+
+public class Daughter extends Parent {
+    public Daughter(int age){
+        this.age = age;
+    }
+    @Override
+    void career(){
+        System.out.println("I want to become a coder");
+    }
+    @Override
+    void partner(){
+        System.out.println("I love Iron Man");
+    }
+    
+}
