a4d328a3dca224d8d0f38288d05e6432e1ec4a3a
AddagiriGanesh123 <150156459+AddagiriGanesh123@users.noreply.github.com>
2025-03-25 08:01:06 +0530

    Add files via upload

---
+++ Interfaces/Brake.java
@@ -0,0 +1,10 @@
+package Interfaces;
+// Interface: Interface is the term defines that it is an abtract data type that defines set of methods 
+// That implements the classes
+// It is used to achieve Multiple Inheritence
+
+public interface Brake {
+
+    static final int price = 70000;
+    abstract void brake();    
+}
