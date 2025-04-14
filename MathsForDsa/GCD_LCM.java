4619138d9fe06fdb23d9580f374d94fbea0b52f9
AddagiriGanesh123 <150156459+AddagiriGanesh123@users.noreply.github.com>
2025-04-14 07:00:56 +0530

    Add files via upload

---
+++ MathsForDsa/GCD_LCM.java
@@ -0,0 +1,18 @@
+public class GCD_LCM {
+    public static void main(String[] args) {
+        System.out.println(gcd(9, 18));
+        System.out.println(lcm(9,18));
+    }
+    public static int gcd(int a, int b){
+        if(a == 0){
+            return b;
+        }
+        return gcd(b % a, a);
+    }
+    // To find LCM
+    // We know LCM = a * b / GCD
+    // i.e., LCM(9, 18) = 9 * 18 /9 = 162 / 9 = 18
+    public static int lcm(int a, int b){
+        return (a * b) / gcd(a, b);
+    }
+}
