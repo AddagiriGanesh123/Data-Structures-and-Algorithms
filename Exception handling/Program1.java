9aadbf76f9472d7bf7a68d0355e499160f0346f3
AddagiriGanesh123 <150156459+AddagiriGanesh123@users.noreply.github.com>
2025-05-26 11:30:14 +0530

    Add files via upload

--- Exception handling/Program1.java
+++ Exception handling/Program1.java
@@ -16,8 +16,14 @@ public class Program1 {
       System.out.println(a);
       int b = 0;
       System.out.println(b);
+      try{
       int c  = a/b;
       System.out.println(c);
+      }
+      catch(ArithmeticException e){
+        System.out.println(e.getMessage());
+      }finally{
       System.out.println("Program Stopped!");
+      }
     }
 }
